8199c4696d9db336988ba2838d68fb1f88921345
chrisvenator <christopher.scherling@gmail.com>
2021-04-10 17:13:49 +0200

    init sights per month

---
+++ src/components/SightsPerMonth(Season).vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "SightsPerMonth(Season)",
+  components: {},
+  data() {
+    return {
+      data: undefined
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
