db34aeb8ab983e6c9c3674ae84dce161ce1f9957
chrisvenator <christopher.scherling@gmail.com>
2021-04-10 18:27:06 +0200

    sichten pro Monat fetrig gestellt

---
+++ src/components/SightsPerMonth-Season.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <figure className="highcharts-figure">
+      <div id="container"></div>
+    </figure>
+  </div>
+</template>
+
+<script>
+import Highchart from 'highcharts/highcharts.js'
+
+
+export default {
+  name: "SightsPerMonth",
+  data() {
+    return {
+      data: undefined
+    }
+  },
+  mounted() {
+    this.getData()
+  },
+  methods: {
+    getData() {
+      this.data = []
+      this.data.januar = 20
+      this.basicChart()
+    },
+    basicChart() {
+      Highchart.chart('container', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'In welchen Monaten werden UFOs meistens beobachtet?'
+        },
+        xAxis: {
+          type: 'category',
+          labels: {
+            rotation: -45,
+            style: {
+              fontSize: '13px',
+              fontFamily: 'Verdana, sans-serif'
+            }
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Sightings (millions)'
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        tooltip: {
+          pointFormat: 'Sightings: <b>{point.y:.1f} millions</b>'
+        },
+        series: [{
+          name: 'Sightings',
+          data: [
+            ['Januar', this.data.januar],
+            ['Februar', 20.8],
+            ['März', 14.9],
+            ['April', 13.7],
+            ['Mai', 13.1],
+            ['Juni', 12.7],
+            ['Juli', 12.4],
+            ['August', 12.2],
+            ['September', 12.0],
+            ['Oktober', 11.7],
+            ['November', 11.5],
+            ['Dezember', 11.2]
+          ],
+          dataLabels: {
+            enabled: true,
+            rotation: -90,
+            color: '#FFFFFF',
+            align: 'right',
+            format: '{point.y:.1f}', // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+              fontSize: '13px',
+              fontFamily: 'Verdana, sans-serif'
+            }
+          }
+        }]
+      });
+    }
+  }
+}
+</script>
+<style>
+#container {
+  height: 400px;
+}
+
+.highcharts-figure, .highcharts-data-table table {
+  min-width: 310px;
+  max-width: 800px;
+  margin: 1em auto;
+}
+
+.highcharts-data-table table {
+  font-family: Verdana, sans-serif;
+  border-collapse: collapse;
+  border: 1px solid #EBEBEB;
+  margin: 10px auto;
+  text-align: center;
+  width: 100%;
+  max-width: 500px;
+}
+
+.highcharts-data-table caption {
+  padding: 1em 0;
+  font-size: 1.2em;
+  color: #555;
+}
+
+.highcharts-data-table th {
+  font-weight: 600;
+  padding: 0.5em;
+}
+
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+  padding: 0.5em;
+}
+
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+  background: #f8f8f8;
+}
+
+.highcharts-data-table tr:hover {
+  background: #f1f7ff;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="MarioHaltsMaul">
+    <!--    <sights-per-month-season></sights-per-month-season>-->
+    <SightsMap></SightsMap>
   </div>
 </template>
+<script>
+// import SightsPerMonthSeason from "@/components/SightsPerMonth-Season";
+import SightsMap from "@/components/SightsMap";
+
+export default {
+  // components: {SightsPerMonthSeason}
+  components: {SightsMap}
+}
+</script>
