79af3abc78d285cc1b9298dde86207a18958ba9f
Mario Seidl <mario.s-@live.de>
2021-04-10 18:51:39 +0200

    loading of Json data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <nav id="nav">
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
-    </div>
+    </nav>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      data: undefined,
+      schmutz1: "hallo"
+    }
+  },
+  created() {
+    this.loadJSON()
+  },
+  methods: {
+    test(zahl) {
+      return zahl
+    },
+    loadJSON() {
+      Axios.get(process.env.BASE_URL + 'daten.json')
+          .then(result => {
+            this.data = result.data
+          })
+    }
+
+  }
+}
+</script>
 
 <style lang="scss">
 @tailwind base;
