a38f5ae59431318046b8589916c86251827e2529
Kristian <5115@htl.rennweg.at>
2021-04-10 17:13:13 +0200

    einfaches pie chart

---
+++ src/components/ChartByCountry.vue
@@ -0,0 +1,98 @@
+<template>
+  <figure class="highcharts-figure">
+    <div id="container"></div>
+    <p class="highcharts-description">
+      Pie charts are very popular for showing a compact overview of a
+      composition or comparison. While they can be harder to read than
+      column charts, they remain a popular choice for small datasets.
+    </p>
+  </figure>
+</template>
+
+<script>
+import Highchart from 'highcharts/highcharts.js'
+
+
+export default {
+  name: "ChartByCountry",
+  data(){
+    return{
+      data: undefined
+    }
+  },
+  mounted() {
+    this.basicChart()
+  },
+  methods: {
+    basicChart() {
+      Highchart.chart('container', {
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+        },
+        title: {
+          text: 'Browser market shares in January, 2018'
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+          point: {
+            valueSuffix: '%'
+          }
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+          }
+        },
+        series: [{
+          name: 'Brands',
+          colorByPoint: true,
+          data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+          }, {
+            name: 'Internet Explorer',
+            y: 11.84
+          }, {
+            name: 'Firefox',
+            y: 10.85
+          }, {
+            name: 'Edge',
+            y: 4.67
+          }, {
+            name: 'Safari',
+            y: 4.18
+          }, {
+            name: 'Sogou Explorer',
+            y: 1.64
+          }, {
+            name: 'Opera',
+            y: 1.6
+          }, {
+            name: 'QQ',
+            y: 1.2
+          }, {
+            name: 'Other',
+            y: 2.61
+          }]
+        }]
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <chart-by-country></chart-by-country>
   </div>
 </template>
+<script>
+import ChartByCountry from "@/components/ChartByCountry";
+export default {
+  components: {ChartByCountry}
+}
+</script>
